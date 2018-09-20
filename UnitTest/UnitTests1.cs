9c09b4a98e9be08e5d5c7e8e188df30fe756018e
Laurent Lagamme <laurentlagamme@MacBook-Pro-de-Laurent.local>
2018-09-20 10:34:45 +0200

    rename tests

--- UnitTest/UnitTests1.cs
+++ UnitTest/UnitTests1.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTest
 {
     [TestClass]
-    public class UnitTest1
+    public class UnitTests1
     {
         [TestMethod]
         public void TestMethod1()
